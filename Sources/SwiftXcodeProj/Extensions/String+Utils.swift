182a1f69f0c77e1a152fba083b7fa814785824dd
stackotter <stackotter@stackotter.dev>
2024-12-09 23:35:43 +1000

    Replace arc4random_uniform with standard Int.random

--- Sources/SwiftXcodeProj/Extensions/String+Utils.swift
+++ Sources/SwiftXcodeProj/Extensions/String+Utils.swift
@@ -1,13 +1,5 @@
 import Foundation
 
-#if os(Linux)
-    import SwiftGlibc
-
-    public func arc4random_uniform(_ max: UInt32) -> Int32 {
-        (SwiftGlibc.rand() % Int32(max - 1))
-    }
-#endif
-
 extension String {
     public var quoted: String {
         "\"\(self)\""
@@ -22,8 +14,8 @@ extension String {
         var randomString: String = ""
 
         for _ in 0 ..< length {
-            let randomValue = arc4random_uniform(UInt32(base.count))
-            randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
+            let randomValue = Int.random(in: 0..<base.count)
+            randomString += "\(base[base.index(base.startIndex, offsetBy: randomValue)])"
         }
         return randomString
     }
